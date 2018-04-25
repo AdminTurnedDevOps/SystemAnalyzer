0aab3ef272971c00962c7c0bbacfc5fa0128ddeb
syncleft <20546397+syncleft@users.noreply.github.com>
2018-04-25 19:37:25 -0400

    added if statement

--- systemanalyzer.py
+++ systemanalyzer.py
@@ -1,5 +1,4 @@
-def main():
-    """
+"""
     The "Scanner" logic will go here
 
     Check os
@@ -10,7 +9,17 @@ def main():
     else
     run Bash Scripts
 
+    The "Scanner" logic will go here
 
+    Check os
+
+    if os == windows
+    call PowerShell
+
+    else
+    run Bash Scripts
+
+                 [Remove kitty before executing script]
                          ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
                        ▄▀░░░░░░░░░░░░▄░░░░░░░▀▄
                        █░░▄░░░░▄░░░░░░░░░░░░░░█
@@ -23,7 +32,16 @@ def main():
                       ▄█▄░░░░░░░░░░░▀▄░░▀▀▀▀▀▀▀▀░░▄▀
                      █▀▀█████████▀▀▀▀████████████▀
                      ████▀  ███▀      ▀███  ▀██▀
-    """
+    
+"""
 
-
+import platform 
+def main():
+    opSys1 = platform.system()
+    if opSys1 == 'Darwin':
+        print("You're on a mac")
+    elif opSys1 == 'Windows':
+        print("You're on a window")
+    elif opSys1 == 'Linux' or opSys1 == 'Linux2':
+        print("You're on a linux machine")
 main()
