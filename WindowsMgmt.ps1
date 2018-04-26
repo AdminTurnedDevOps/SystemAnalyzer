d9ab08e723b687c4cfff1bb05ee4a243f35c1813
Mike Levan <mlevan1992@gmail.com>
2018-04-26 12:49:01 -0400

    Get disk space, free disk space, partitions, and model of hard drive

--- WindowsMgmt.ps1
+++ WindowsMgmt.ps1
@@ -1,1 +1,11 @@
-
+$FreeDiskSpace = Get-CimInstance -ClassName Win32_LogicalDisk
+$DiskDrive = Get-CimInstance -ClassName Win32_DiskDrive
+Foreach ($disk in $DiskDrive) {
+    $DiskDriveOBJECT = [pscustomobject] @{
+        'DiskSize'       = $Disk.Size / 1GB
+        'FreeDiskSpace'  = $FreeDiskSpace.FreeSpace / 1GB
+        'DiskPartitions' = $DiskDrive.partitions
+        'DiskModel'      = $DiskDrive.Model
+    }
+    $DiskDriveOBJECT
+}#Foreach
