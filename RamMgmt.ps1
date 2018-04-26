dd4875ab26a4df92d3eb2f6a3858458cb026b64e
Mike Levan <mlevan1992@gmail.com>
2018-04-26 17:22:40 -0400

    Itty bitty function to retrieve RAM size

---
+++ RamMgmt.ps1
@@ -0,0 +1,15 @@
+Function Get-RAMSize {
+    Try {
+$RamCIM = Get-CimInstance -ClassName Win32_PhysicalMemory | 
+          Select @{Name='size' ;expression={[math]::Round($_.Capacity / 1GB)}}
+Write-Output 'Below is your total memory'
+$RamCIM
+    }
+
+    Catch {
+        Write-Warning 'An error has occured..'
+        Pause
+        $_
+        Throw
+    }
+}#Function
