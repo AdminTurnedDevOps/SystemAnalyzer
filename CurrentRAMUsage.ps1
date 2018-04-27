6e967d59207454688420841dc969be5af90fbf12
Mike Levan <mlevan1992@gmail.com>
2018-04-27 08:06:07 -0400

    Core code for currently utilized RAM

--- CurrentRAMUsage.ps1
+++ CurrentRAMUsage.ps1
@@ -1,3 +1,8 @@
-$Mem = Get-Ciminstance Win32_OperatingSystem | Select @{Name='FreeRAM' ;expression={[math]::Round($_.FreePhysicalMemory / 1MB)}},
-                                                      @{Name='MaxRAMSize' ;expression={[math]::Round($_.MaxProcessMemorySize / 1GB)}}
+Function CurrentRAMUsage {
+
+$Mem = Get-Ciminstance Win32_OperatingSystem | 
+       Select-Object @{Name='FreeRAM' ;expression={[math]::Round($_.FreePhysicalMemory / 1MB)}},
+                     @{Name='MaxRAMSize' ;expression={[math]::Round($_.MaxProcessMemorySize / 1GB)}}
 $Mem
+}#Function
+CurrentRAMUsage
