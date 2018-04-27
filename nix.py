5a549cd2beab689cbee4f6086727d111f829ad13
Chris <chris@admindev.tech>
2018-04-26 19:25:17 -0500

    Added 'titles' and RAM usage to program

--- nix.py
+++ nix.py
@@ -4,4 +4,11 @@ import subprocess as s
 
 def bash():
     # Bar
+    print("Disk Usage")
+    print("_________________________")
     disk = s.run(["df", "-h"])
+    print()
+    print()
+    print("RAM")
+    print("_________________________")
+    mem = s.run(["free", "-h"])
