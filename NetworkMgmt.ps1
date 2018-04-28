c6196254846a68ddc98430b6d18bacbeabf000cf
Mike Levan <mlevan1992@gmail.com>
2018-04-28 09:09:51 -0400

    Network Management - Cleaned up code, put objects into one custom object, and added foreach.

--- NetworkMgmt.ps1
+++ NetworkMgmt.ps1
@@ -1,28 +1,22 @@
 Function NetProtocols {
+    $NetworkProtocol = Get-CimInstance -ClassName Win32_NetworkProtocol
+    $NetworkAdapter = Get-CimInstance -ClassName Win32_NetworkAdapter
+    $NetworkPerformance = Get-CimInstance -ClassName Win32_PerfFormattedData_Tcpip_NetworkInterface
+    $NET = $NetworkProtocol + $NetworkAdapter + $NetworkPerformance
 
-$NetworkProtocol = Get-CimInstance -ClassName Win32_NetworkProtocol
-$NetworkAdapter = Get-CimInstance -ClassName Win32_NetworkAdapter
-$NetworkPerformance = Get-CimInstance -ClassName Win32_PerfFormattedData_Tcpip_NetworkInterface
+    Write-Host -ForegroundColor Green "Below is a list of;`nNetwork Protocols`nNetwork Adapters`nService Names`nBandwidth Information`nNetwork adapter you are currently using"
+    
+    Foreach ($Protocol in $NET) {
+        $NetOBJECTS = [pscustomobject] @{
+            'NetworkProtocol'          = $Protocol.Name
+            'ProtocolCaption'          = $Protocol.Caption
+            'NetworkAdapters'          = $Protocol.Name
+            'NetAdapterServiceName'    = $Protocol.ServiceName
+            'Name'                     = $Protocol.Name
+            'BytesReceivedPerSecond'   = $Protocol.BytesReceivedPersec
+            'CurrentUtilizedBandwidth' = $Protocol.CurrentBandwidth
+        }
 
-Write-Host -ForegroundColor Green 'Below are your current network protocols being utilized by your system'
-$NetworkOBJECT = [pscustomobject] @{
-    'NetworkProtocol' = $NetworkProtocol.Name
-    'ProtocolCaption' = $NetworkProtocol.Caption
-}
-Write-Host -ForegroundColor 'Below are your current network adapters and service names'
-$NetworkOBJECT
-
-$NetworkOBJECT2 = [pscustomobject] @{
-    'NetworkAdapters'       = $NetworkAdapter.Name
-    'NetAdapterServiceName' = $NetworkAdapter.ServiceName
-}
-$NetworkOBJECT2
-
-$NetworkOBJECT3 = [pscustomobject] @{
-    'Name'                     = $NetworkPerformance.Name
-    'BytesReceivedPerSecond'   = $NetworkPerformance.BytesReceivedPersec
-    'CurrentUtilizedBandwidth' = $NetworkPerformance.CurrentBandwidth
-}
-Write-Host -ForegroundColor Green 'Below is bandwidth information for the adapter you are currently using'
-$NetworkOBJECT3
-}#Function
+        $NetOBJECTS
+    }
+}#function
