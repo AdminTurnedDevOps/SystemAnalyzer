e6ac7f2d0016002a340af1c62079b93cca606162
Mike Levan <mlevan1992@gmail.com>
2018-04-30 09:18:41 -0400

    Update DiskMgmt.ps1

--- PowerShell/DiskMgmt.ps1
+++ PowerShell/DiskMgmt.ps1
@@ -10,22 +10,10 @@ Function Get-DiskStats {
     Process {
         Try {
 <<<<<<< HEAD:PowerShell/DiskMgmt.ps1
-            $FreeDiskSpace = Get-CimInstance -ClassName Win32_LogicalDisk
-            $DiskDrive = Get-CimInstance -ClassName Win32_DiskDrive
-
-            Foreach ($disk in $DiskDrive) {
-
-                $DiskDriveOBJECT = [pscustomobject] @{
-                    'DiskSize'       = $Disk.Size / 1GB
-                    'FreeDiskSpace'  = $FreeDiskSpace | Select C:,D:,E:,F:,@{Name='FreeSpace' ;Expression={[math]::Round($_.FreeSpace / 1GB)}}
-                    'DiskPartitions' = $Disk.Partitions
-                    'DiskModel'      = $Disk.Model
-                }
-               $DiskDriveOBJECT
-            }#Foreach
-=======
            $Disk = Get-CimInstance -ComputerName $ComputerName -ClassName Win32_LogicalDisk
            $Disk
+=======
+
 >>>>>>> f603fa9a355d9f40e430a92acea6481c1cc0c5c9:DiskMgmt.ps1
         }
         Catch {
