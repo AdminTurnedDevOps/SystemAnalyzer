1a5f3f033df8489e98e58fc63b488aef9dd83afc
Mike Levan <mlevan1992@gmail.com>
2018-05-02 20:41:38 -0400

    Update CurrentRAMUsage.ps1

--- PowerShell/CurrentRAMUsage.ps1
+++ PowerShell/CurrentRAMUsage.ps1
@@ -3,6 +3,5 @@ Function CurrentRAMUsage {
 $Mem = Get-Ciminstance Win32_OperatingSystem | 
        Select-Object @{Name='FreeRAM' ;expression={[math]::Round($_.FreePhysicalMemory / 1MB)}},
                      @{Name='MaxRAMSize' ;expression={[math]::Round($_.MaxProcessMemorySize / 1GB)}}
-$Mem
 }#Function
 CurrentRAMUsage
